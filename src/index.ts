b3db95ef0881dfed6a43e9a82cd64d6c1ac9863b
Hannes Lindenberg <hanneslinde@posteo.de>
2021-03-31 09:36:23 +0000

    set the basis for now setting up a test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { makepuzzle } from 'sudoku';
+
 export const add = function (a: number, b: number) {
   return a + b;
 }
+
+type Cell = null | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+type Sudoku = Cell[];
+
+const stringifySudoku = function (sudoku: Sudoku): string {
+  return ""
+}
+
+const puzzle: Sudoku = makepuzzle();
+
+console.log(stringifySudoku(puzzle));
