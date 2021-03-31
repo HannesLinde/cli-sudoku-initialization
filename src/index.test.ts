7ed3654ef4f17955c17603e9e23e4aba315f9410
Hannes Lindenberg <hanneslinde@posteo.de>
2021-03-31 13:10:00 +0000

    start a createLinebreak function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,18 @@
-import { add } from "./index"
+import { stringifySudoku, Sudoku } from "./index"
 
-test("add to integers", function () {
-  expect(add(1, 5)).toEqual(6);
+test("convert a sudoku board inbto a pretty string", function () {
+  const puzzle: Sudoku = [
+    null, 4, null, null, null, 3, 1, null, null, null,
+    null, 2, null, null, null, null, null, 0, 5, null,
+    null, null, 8, null, null, 4, 7, 1, null, null,
+    null, 5, null, 8, null, 4, null, null, null, null,
+    null, 8, null, null, null, null, null, null, 6, 4,
+    null, 2, 1, null, null, null, null, null, null, null,
+    3, null, 1, null, 6, null, null, null, null, null,
+    null, null, 7, null, 8, 3, null, 6, null, null,
+    null
+  ];
+  const puzzleAsString = " 5 42 \n 3 16 \n 9 582 \n 6 9 5 \n 9 75\n 32 \n4 2 7 \n 8 94 7 ";
+
+  expect(stringifySudoku(puzzle)).toEqual(puzzleAsString);
 })
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
 import { makepuzzle } from 'sudoku';
 
-export const add = function (a: number, b: number) {
-  return a + b;
-}
-
-type Cell = null | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-type Sudoku = Cell[];
-
-const stringifySudoku = function (sudoku: Sudoku): string {
-  return ""
-}
+export type Cell = null | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+export type Sudoku = Cell[];
 
 const puzzle: Sudoku = makepuzzle();
 
-console.log(stringifySudoku(puzzle));
+const createLinebreaks = function (puzzle: Sudoku) {
+  let rows = [];
+  let sliceEnd: number
+  for (let i = 1; i < 82; i += 1) {
+    if (i % 9 === 0) {
+      let sliceStart: number = (i < 10) ? 0 : sliceEnd;
+      sliceEnd = i;
+      // console.log(sliceStart, sliceEnd);
+      rows.push(puzzle.slice(sliceStart, sliceEnd));
+      sliceStart = sliceEnd;
+      // console.log(rows);
+      // console.log(sliceStart, sliceEnd);
+    };
+  }
+
+  return rows;
+}
+
+createLinebreaks(puzzle);
+
+const cellToPrettyString = function (cell: Cell): string {
+  if (cell === null) {
+    return " ";
+  }
+  return (cell + 1).toString();
+}
+
+export const stringifySudoku = function (sudoku: Sudoku): string {
+  return rows.map(cellToPrettyString).join("");
+}
+
+
+// console.log(stringifySudoku(puzzle));
